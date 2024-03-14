70ae18cb372bdc632da0984dcced667aebdeed83
Priyanshu0007 <priyanshu2859@gmail.com>
2024-03-14 23:36:54 +0530

    second commit

--- screen/Home.js
+++ screen/Home.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import {MagnifyingGlassIcon, MapPinIcon} from 'react-native-heroicons/outline'
 import {theme} from "../theme/index"
+import Icon from "react-native-vector-icons/Fontisto"
 export default function Home() {
     const [showSearch,toggleSearch]=useState(false);
     const [location,setLocation]=useState([1,2,3]);
@@ -18,7 +19,7 @@ export default function Home() {
                         <TextInput className='pl-6 h-10 flex-1 text-base text-white' placeholder='Search city ...' placeholderTextColor={'lightgray'} ></TextInput>
                     ):null}
                     <TouchableOpacity onPress={()=>toggleSearch(!showSearch)} style={{backgroundColor:theme.bgWhite(0.7)}} className='rounded-full p-3 m-1'>
-                        <MagnifyingGlassIcon size={25} />
+                        <MagnifyingGlassIcon size={25} color={"gray"}/>
                     </TouchableOpacity>
                 </View>
                 {location.length>0 && showSearch?(
@@ -29,14 +30,31 @@ export default function Home() {
                                 let borderClass= showBorder ? "border-b-2 border-b-gray-400": null;
                                 return (
                                     <TouchableOpacity onPress={handelLocation(loc)} key={index} className={"flex-row items-center border-0 p-3 px-4 mb-1 "+borderClass}>
-                                        <MapPinIcon/>
-                                        <Text>London, United Kingdom</Text>
+                                        <MapPinIcon size={20} color={'gray'}/>
+                                        <Text className="text-black text-lg ml-2">London, United Kingdom</Text>
                                     </TouchableOpacity>
                                 )
                             })
                         }
                     </View>
                 ):null}
+            </View>
+            <View className="mx-4 flex justify-around flex-1 mb-2">
+                <Text className='text-white text-center text-2xl font-bold'>
+                    London, <Text className="text-lg font-semibold text-gray-300">United Kingdom</Text>
+                </Text>
+                <View className='flex-row justify-center'>
+                    <Icon size={100} color={"white"} name="day-cloudy"/>
+                </View>
+                <View className='space-y-2'>
+                    <Text className='text-center font-bold text-white text-6xl ml-5'>26&#176;</Text>
+                    <Text className='text-center text-white text-xl ml-5 tracking-widest'>Partly Cloudy</Text>
+                </View>
+                <View className="flex-row justify-between mx-4">
+                    <View className='flex-row space-x-2 items-center'>
+                        
+                    </View>
+                </View>
             </View>
         </SafeAreaView>
     </View>
